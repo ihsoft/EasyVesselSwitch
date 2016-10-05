da798556b229db79828e44b1ae1aca39d00f3c67
ihsoft <igor.zavoychinskiy@gmail.com>
2016-10-05 00:01:16 -0700

    Pre-release v1.1.1

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -24,8 +24,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion ("1.1.0")]
-[assembly: AssemblyInformationalVersion ("1.1.0-pre for KSP v1.2")]
+[assembly: AssemblyVersion ("1.1.1")]
+[assembly: AssemblyInformationalVersion ("1.1.1-pre for KSP v1.2-pre")]
 [assembly: Guid ("52fd90de-a2a6-4642-8fc4-50f8d938319c")]
 [assembly: KSPAssembly ("EasyVesselSwitch", 1, 1)]
 [assembly: AssemblyFlags (AssemblyNameFlags.PublicKey | AssemblyNameFlags.EnableJITcompileOptimizer)]
