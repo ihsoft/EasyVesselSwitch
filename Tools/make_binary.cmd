31950e01520af59b0f232b09d75c1952ada0d1a1
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2021-01-19 22:23:49 -0800

    Update release scripts

--- Tools/make_binary.cmd
+++ Tools/make_binary.cmd
@@ -1,3 +1,3 @@
 @echo off
 REM EVS *requires* .NET compiler version 6.0 or higher and Framework version not less than 4.5.
-"C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe" ..\Source\EasyVesselSwitch.csproj /t:Clean,Build /p:Configuration=Release
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\amd64\MSBuild.exe" ..\Source\EasyVesselSwitch.csproj /t:Clean,Build /p:Configuration=Release
